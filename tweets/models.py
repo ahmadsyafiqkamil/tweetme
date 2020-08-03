59561ffaf4a58800a5c1d075f7485e9029e2a336
ahmadsyafiqkamil <ahmadsyafiqkamil@gmail.com>
2020-08-03 22:48:08 +0800

    append tweet and reorder

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 # Create your models here.
 
@@ -6,4 +7,10 @@ class Tweet(models.Model):
     content = models.TextField(blank=True,null=True)
     image = models.FileField(upload_to='image/',blank=True,null=True)
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0,100)
+        }
 
--- tweets/views.py
+++ tweets/views.py
@@ -24,7 +24,7 @@ def tweet_create_view(request, *args, **kwargs):
         obj = form.save(commit=False)
         obj.save()
         if request.is_ajax():
-            return JsonResponse({},status=201)
+            return JsonResponse(obj.serialize(),status=201)
         if next_url != None and is_safe_url(next_url,ALLOWED_HOST):
             return redirect(next_url)
         form = TweetForm()
@@ -33,7 +33,7 @@ def tweet_create_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{'id': x.id, 'content': x.content, 'likes': random.randint(0, 1234)} for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         'isUser': False,
         'response': tweets_list

f07795b68e018a893c567f9dac1b4709609109bc
ahmadsyafiqkamil <ahmadsyafiqkamil@gmail.com>
2020-08-08 22:30:57 +0800

    handling cors

--- tweetme-web/src/App.js
+++ tweetme-web/src/App.js
@@ -6,11 +6,15 @@ import './App.css';
 function App() {
     const [tweet, setTweet] = useState([])
     useEffect(() => {
-
-
-
-        const tweetItem = [{"content": 123}, {"content": 123134}]
-        setTweet(tweetItem)
+        const myCallback = (response, status) => {
+            console.log(response, status)
+            if (status === 200) {
+                setTweet(response)
+            }else{
+                alert("error")
+            }
+        }
+        loadTweets(myCallback)
     }, [])
 
     return (
@@ -39,12 +43,16 @@ function App() {
 function loadTweets(callback) {
     const xhr = new XMLHttpRequest();
     const method = 'GET';
-    const url = "/tweets";
+    const url = "http://127.0.0.1:8000/api/tweets/";
     const respType = "json";
     xhr.responseType = respType;
     xhr.open(method, url);
     xhr.onload = function () {
         callback(xhr.response, xhr.status)
+    }
+    xhr.onerror= function (e){
+        console.log(e)
+        callback({"message":"error"},400)
     }
     xhr.send();
 }
